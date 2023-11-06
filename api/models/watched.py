5e2b7988b5c3b8e07ce12ec659638a65922f27de
Mitchell <mitchellzubich@gmail.com>
2023-11-05 19:26:46 -0500

    Clean setup

--- api/models/watched.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, DateTime, Integer, String
-from models.base import Base
-
-class Watched(Base):
-    __tablename__ = 'watched_movies'
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, unique=True, index=True)
-    watched_date = Column(DateTime)
